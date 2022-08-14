b039f134f39e466774110c7987de81384ce84455
ferryboy001 <tc.galaxy@icloud.com>
2022-08-14 22:52:33 +0800

    defined operateUI() function

--- js/ui.js
+++ js/ui.js
@@ -1,14 +1,4 @@
-function showOrHide(id, operation) {
-  /**
-   * @param {string} the id of element that will be operated.
-   * @param {string} "hide" or "show" - refers to operation that will be applied to the element
-   * @return {void}
-   */
-  let control = document.getElementById(id);
-  if (operation == "show") {
-    control.classList.remove("hidden");
-    control.focus();
-  } else if (operation == "hide") {
-    control.classList.add("hidden");
-  }
+function operateUI(selector, callback) {
+  let elementCollection = document.querySelectorAll(selector);
+  callback(elementCollection);
 }
